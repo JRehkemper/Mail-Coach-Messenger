7ee81b904d11866a7bbaeed07a8208f1f14c1f2d
root <root@chatroom.upc.de>
2021-01-26 13:37:44 +0000

    chatgerüst

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
