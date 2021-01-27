99a206172666d7fc37c51ee962d46a23d4e021b0
JadBlackstone <jannik.rehkemper@gmail.com>
2021-01-27 09:21:23 +0100

    bugfixing

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
+
+/*
+Possible Bootstrap fix
+https://www.educative.io/edpresso/how-to-add-bootstrap-to-your-nodejs-project
+*/
+/*
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+
+app.use("/js", 
+  express.static(path.join(__dirname, "node_modules/jquery/dist"))
+);
+
+app.use("/js",
+  express.static(path.join(__dirname, "node_modules/popper.js"))
+);
+
+global.jQuery = require('jquery');
+require('bootstrap');*/
+
+/*
+End of Fix
+*/
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
