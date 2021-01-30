043e6f1ff8879cd3dd8a316c85ccc5eb96f71c52
JadBlackstone <jannik.rehkemper@gmail.com>
2021-01-30 17:17:50 +0100

    WIP multiple Rooms

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
 const { disconnect } = require('process');
 var usercount = 0;
+var reqRoom;
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+  console.log("recieved Get Request");
+  //console.log(document.cookie);
+  console.log(req.cookies['username']);
+  reqRoom = req.cookies['room'];
+  console.log(reqRoom);
+  if(reqRoom == null)
+  {
+    reqRoom = "main";
+  }
 });
 
 http.listen(3000, () => {
@@ -17,16 +29,16 @@ http.listen(3000, () => {
 });
 
 io.on('connection', (socket) => {
-  socket.join("main");
+  socket.join(reqRoom);
   console.log('a user connected');
-  io.sockets.in("main").emit('connection');
+  io.sockets.in(reqRoom).emit('connection');
   usercount++;
-  io.sockets.in("main").emit('usercount', usercount);
+  io.sockets.in(reqRoom).emit('usercount', usercount);
   socket.on('disconnect', (socket) => {
     console.log('a user left');
-    io.sockets.in("main").emit('disconnected');
+    io.sockets.in(reqRoom).emit('disconnected');
     usercount--;
-    io.sockets.in("main").emit('usercount', usercount);
+    io.sockets.in(reqRoom).emit('usercount', usercount);
   })
   
   /*socket.on('refresh', (socket) => {
@@ -37,14 +49,18 @@ io.on('connection', (socket) => {
   });*/
 
   socket.on('chat message', (msg, username, proomname) => {
-    if(proomname == "") {
+    /*if(proomname == "") {
       roomname = "main";
     }
     else {
       roomname = proomname;
-    }
-    io.sockets.in(roomname).emit('chat message', msg, username);
-      console.log('chat message: ' + roomname +" "+ msg + " " + proomname);
+    }*/
+    //roomname = reqRoom;
+    var roomlist = io.sockets.adapter.rooms; // liste aller Räume
+    console.log(roomlist); //log aller Räume
+
+    io.sockets.in(proomname).emit('chat message', msg, username);
+      console.log('chat message: ' + proomname +" "+ msg + " ");
   });
 
   socket.on('newRoomCreate', (roomName) => {
